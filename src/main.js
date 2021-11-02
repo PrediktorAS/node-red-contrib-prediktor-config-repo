21297b22fb409c73a2c8ac32fddc30f9fa3e10d6
Andreas Lydersen <andreas.lydersen@prediktor.com>
2021-11-02 13:18:33 +0100

    Renaming from get-all to get-nodes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const prediktorConfigRepository = require("./nodes/prediktor-config-repository")
 ping = require("nodes/ping");
 uploadContent = require("nodes/upload-content");
 downloadContent = require("nodes/download-content");
-getAll = require("nodes/get-all");
+getNodes = require("nodes/get-nodes");
 get = require("nodes/get");
 create = require("nodes/create");
 update = require("nodes/update");
@@ -15,7 +15,7 @@ module.exports = function(RED){
     RED.nodes.registerType('ping', ping.PingNode);
     RED.nodes.registerType('upload-content', uploadcontent.uploadContentNode);
     RED.nodes.registerType('download-content', downloadcontent.downloadContentNode);
-    RED.nodes.registerType('get-all', getAll.GetAllNode);
+    RED.nodes.registerType('get-nodes', getNodes.GetAllNodes);
     RED.nodes.registerType('get', get.GetNode);
     RED.nodes.registerType('create', create.CreateNode);
     RED.nodes.registerType('update', update.UpdateNode);
